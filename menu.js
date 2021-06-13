2e549a79db7c1d52b36eb7f780ad865ab27d9e2b
Omowunmi Adesuyi <adesuyiomowunmi@gmail.com>
2021-06-13 17:10:07 +0100

    updated

--- menu.js
+++ menu.js
@@ -1,11 +1,12 @@
 const toggleButton = document.getElementById('navLinks')
 
 toggleButton.addEventListener('click', function () {
-    let navLinksWrapper = document.getElementsByClassName('linkks')[0]
+    let navLinksWrapper = document.getElementsByClassName('sidebar_links')[0]
+    console.log(navLinksWrapper);
     
-    if (navLinksWrapper.style.visibility == 'hidden') {
-        navLinksWrapper.style.visibility = 'visible'
-    } else {
-        navLinksWrapper.style.visibility = 'hidden'
-    }
+    // if (navLinksWrapper.style.visibility == 'hidden') {
+    //     navLinksWrapper.style.visibility = 'visible'
+    // } else {
+    //     navLinksWrapper.style.visibility = 'hidden'
+    // }
 })
